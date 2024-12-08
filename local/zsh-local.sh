ab99f8eb395e9d94bad19bedff55fdf0561ee19e
Pavel <paveltessman@gmail.com>
2024-12-08 22:52:05 +0300

    Create zsh-local.sh

---
+++ local/zsh-local.sh
@@ -0,0 +1,12 @@
+alias keygen="~/scripts/keygen.sh"
+alias obscamera="sudo modprobe v4l2loopback"
+
+alias javac="javac -cp .:~/.java/algs4.jar -d ./.javaclasses"
+alias java="java -cp .:~/.java/algs4.jar:./.javaclasses"
+
+export PATH=$PATH:/usr/local/go/bin
+export PAGER="nvim -"
+
+#gradle stuff
+export GRADLE_HOME="/usr/local/gradle/gradle-8.10"
+export PATH=$GRADLE_HOME/bin:$PATH
