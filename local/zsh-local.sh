139cc0e6a2f152b833b517fbd801d08a786269a7
Pavel <tessmanpavel@gmail.com>
2024-12-20 21:30:21 +0300

    Remove gradle stuff

--- local/zsh-local.sh
+++ local/zsh-local.sh
@@ -6,7 +6,3 @@ alias java="java -cp .:~/.java/algs4.jar:./.javaclasses"
 
 export PATH=$PATH:/usr/local/go/bin
 export PAGER="nvim -"
-
-#gradle stuff
-export GRADLE_HOME="/usr/local/gradle/gradle-8.10"
-export PATH=$GRADLE_HOME/bin:$PATH
